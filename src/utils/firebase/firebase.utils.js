8498da7236da3646dfad3a6b0df6a9c2c03015b7
joybd97 <maniaahmedjoy@gmail.com>
2022-04-20 17:35:21 +0600

    Firebase Added

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,39 @@
+import {initializeApp} from 'firebase/app';
+
+import {getAuth,signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAwjldq0IiCAp6aiSd24DtbuQdS6IpBNG0",
+    authDomain: "crown-clothing-db-cbf09.firebaseapp.com",
+    projectId: "crown-clothing-db-cbf09",
+    storageBucket: "crown-clothing-db-cbf09.appspot.com",
+    messagingSenderId: "985219992904",
+    appId: "1:985219992904:web:6d9df5e66e3538383eb77d"
+  };
+  
+  // Initialize Firebase
+  const firebaseapp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+      prompt: "select_account"
+  });
+
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db,'user',userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+}
