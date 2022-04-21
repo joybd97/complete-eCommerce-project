fabc49ec9367c56d4223d043a417039e6d003dc7
joybd97 <maniaahmedjoy@gmail.com>
2022-04-21 17:32:58 +0600

    button and form component updated

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app';
 
-import {getAuth,signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getAuth,signInWithRedirect, signInWithPopup, GoogleAuthProvider,createUserWithEmailAndPassword} from 'firebase/auth';
 
 import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
@@ -17,19 +17,20 @@ const firebaseConfig = {
   // Initialize Firebase
   const firebaseapp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
 
-  provider.setCustomParameters({
+  googleProvider.setCustomParameters({
       prompt: "select_account"
   });
 
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+  export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) =>{
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation ={}) =>{
     const userDocRef = doc(db,'user',userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
@@ -41,7 +42,7 @@ export const createUserDocumentFromAuth = async (userAuth) =>{
 
         try {
             await setDoc(userDocRef,{
-                displayName,email,createdAt
+                displayName,email,createdAt, ...additionalInformation
             });
         } catch (error) {
             console.log(error);
@@ -50,4 +51,10 @@ export const createUserDocumentFromAuth = async (userAuth) =>{
     }
 
     return userDocRef;
+}
+
+
+export const createAuthUserWithEmailAndPassword = async(email, password) =>{
+    if(!email || !password) return;
+    return await createUserWithEmailAndPassword(auth,email,password);
 }
