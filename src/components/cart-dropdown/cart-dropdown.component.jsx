53fbcc2e2b3f2823677d5bb3f4b7b564a9998d33
joybd97 <maniaahmedjoy@gmail.com>
2022-04-23 20:34:32 +0600

    Cart item updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './cart-dropdown.style.scss';
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+  
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items"/>
-            <Button>Go to Checkout</Button>
+      <div className='cart-dropdown-container'>
+        <div className='cart-items'>
+          {cartItems.length ? (
+            cartItems.map((cartItem) => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))
+          ) : (
+            <span className='empty-message'>Your cart is empty</span>
+          )}
         </div>
+        <Button>GO TO CHECKOUT</Button>
+      </div>
     );
-};
+  };
+  
 
 export default CartDropdown;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,16 @@ import { CartContext } from '../../context/cart.context';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 const CartIcon = () => {
-    const {isCartOpen,setIsCartOpen} = useContext(CartContext);
-    const toggleIsCartOpen =()=> setIsCartOpen(!isCartOpen);
+    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  
     return (
-        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-            <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
-        </div>
+      <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>0</span>
+      </div>
     );
-};
+  };
 
 export default CartIcon;
